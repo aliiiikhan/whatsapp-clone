802c49ef94c22b7856dd59b3612768946378890c
aliiiikhan <151985827+aliiiikhan@users.noreply.github.com>
2024-09-01 09:02:34 -0700

    Commit

---
+++ lib/common/theme/dark_theme.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:whatsapp_messenger/common/extension/custom_theme_extension.dart';
+import 'package:whatsapp_messenger/common/utils/coloors.dart';
+
+ThemeData darkTheme() {
+  final ThemeData base = ThemeData.dark();
+  return base.copyWith(
+    scaffoldBackgroundColor: Coloors.backgroundDark,
+    extensions: [CustomThemeExtension.darkMode],
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Coloors.greyBackground,
+      titleTextStyle: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
+        color: Coloors.greyDark,
+      ),
+      systemOverlayStyle: SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+      ),
+      iconTheme: IconThemeData(
+        color: Coloors.greyDark,
+      ),
+    ),
+    tabBarTheme: const TabBarTheme(
+      indicator: UnderlineTabIndicator(
+        borderSide: BorderSide(
+          color: Coloors.greenDark,
+          width: 2,
+        ),
+      ),
+      unselectedLabelColor: Coloors.greyDark,
+      labelColor: Coloors.greenDark,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Coloors.greenDark,
+        foregroundColor: Coloors.backgroundDark,
+        splashFactory: NoSplash.splashFactory,
+        elevation: 0,
+        shadowColor: Colors.transparent,
+      ),
+    ),
+    bottomSheetTheme: const BottomSheetThemeData(
+      backgroundColor: Coloors.greyBackground,
+      modalBackgroundColor: Coloors.greyBackground,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(20),
+        ),
+      ),
+    ),
+    dialogBackgroundColor: Coloors.greyBackground,
+    dialogTheme: DialogTheme(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+    ),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: Coloors.greenDark,
+      foregroundColor: Colors.white,
+    ),
+    listTileTheme: const ListTileThemeData(
+      iconColor: Coloors.greyDark,
+      tileColor: Coloors.backgroundDark,
+    ),
+    switchTheme: const SwitchThemeData(
+      thumbColor: MaterialStatePropertyAll(Color(0xFF83939C)),
+      trackColor: MaterialStatePropertyAll(Color(0xFFDADFE2)),
+    ),
+    colorScheme: ColorScheme(
+      brightness: Brightness.dark,
+      primary: Colors.blue, // Replace with your desired color
+      onPrimary: Colors.white, // Replace with your desired color
+      secondary: Colors.blueAccent, // Replace with your desired color
+      onSecondary: Colors.white, // Replace with your desired color
+      error: Colors.red, // Replace with your desired color
+      onError: Colors.white, // Replace with your desired color
+      background: Coloors.backgroundLight,
+      onBackground: Colors.black, // Replace with your desired color
+      surface: Colors.grey, // Replace with your desired color
+      onSurface: Colors.black, // Replace with your desired color
+    ),
+  );
+}
